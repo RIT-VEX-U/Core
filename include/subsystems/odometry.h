3c0bbe6327c654452bc601ecb30e35261d8a9477
Ryan McGee <ramtech51@comcast.net>
2021-07-02 11:26:05 -0400

    Restructured calculations, fixed async and added update()

--- include/subsystems/odometry.h
+++ include/subsystems/odometry.h
@@ -29,6 +29,13 @@ typedef struct
     double wheel_diam;
 } odometry_config_t;
 
+// Contains information stored between update()s required for calculating posiion
+typedef struct
+{
+    position_t pos;
+    double lside, rside;
+} stored_info_t;
+
 class Odometry
 {
     public:
@@ -38,16 +45,20 @@ class Odometry
      * @param left_side The left motors 
      * @param right_side The right motors
      * @param imu The robot's inertial sensor
+     * @param is_async If true, the robot will automatically poll it's position and update it in the background.
+     *      If false, the update() function must be called periodically.
      */
-    Odometry(vex::motor_group &left_side, vex::motor_group &right_side, vex::inertial &imu, odometry_config_t &config);
+    Odometry(vex::motor_group &left_side, vex::motor_group &right_side, vex::inertial &imu, odometry_config_t &config, bool is_async);
 
     /**
      * Initialize the Odometry module, calculating the rotation from encoders
      * @param left_side The left motors 
      * @param right_side The right motors
      * @param imu The robot's inertial sensor
+     * @param is_async If true, the robot will automatically poll it's position and update it in the background.
+     *      If false, the update() function must be called periodically.
      */
-    Odometry(vex::motor_group &left_side, vex::motor_group &right_side, odometry_config_t &config);
+    Odometry(vex::motor_group &left_side, vex::motor_group &right_side, odometry_config_t &config, bool is_async);
 
     /**
      * Gets the current position and rotation
@@ -58,6 +69,15 @@ class Odometry
      * Sets the current position of the robot
      */
     void set_position(position_t &newpos);
+
+    /**
+     * Update the current position on the field based on the sensors
+     */
+    position_t update();
+
+    void end_async();
+
+    bool end_task = false;
 
     /**
      * Get the distance between two points
@@ -71,16 +91,21 @@ class Odometry
 
     private:
 
+    /**
+     * Get information from the input hardware and an existing position, and calculate a new current position
+     */
+    static position_t calculate_new_pos(odometry_config_t &config, stored_info_t &stored_info, double lside_revs, double rside_revs, double *gyro_angle_deg=NULL);
+
     vex::mutex mut;
     position_t current_pos;
 
-    vex::motor_group *left_side, *right_side;
+    vex::motor_group &left_side, &right_side;
     vex::inertial *imu;
-    odometry_config_t *config;
+    odometry_config_t &config;
 
     vex::task *handle;
 
-    
+    stored_info_t stored_info;
     
 };
 
--- src/subsystems/odometry.cpp
+++ src/subsystems/odometry.cpp
@@ -6,15 +6,15 @@
  * @param right_side The right motors
  * @param imu The robot's inertial sensor
  */
-Odometry::Odometry(vex::motor_group &left_side, vex::motor_group &right_side, vex::inertial &imu, odometry_config_t &config)
-: left_side(&left_side), right_side(&right_side), imu(&imu), config(&config)
+Odometry::Odometry(vex::motor_group &left_side, vex::motor_group &right_side, vex::inertial &imu, odometry_config_t &config, bool is_async)
+: left_side(left_side), right_side(right_side), imu(&imu), config(config)
 {
-    // Create a raw pointer containing a list of all the hardware pointers needed to run the thread.
-    // This is done because callbacks do not allow class member functions
-    void* list[] = {this->left_side, this->right_side, this->imu, this->config, &mut, &current_pos};
+    // Make sure the last known info starts zeroed
+    memset(&stored_info, 0, sizeof(stored_info_t));
 
-    // Initialize and start the background task
-    handle = new vex::task(background_task, list);
+    // Start the asynchronous background thread
+    if (is_async)
+        handle = new vex::task(background_task, this);
 }
 
 /**
@@ -23,95 +23,70 @@ Odometry::Odometry(vex::motor_group &left_side, vex::motor_group &right_side, ve
  * @param right_side The right motors
  * @param imu The robot's inertial sensor
  */
-Odometry::Odometry(vex::motor_group &left_side, vex::motor_group &right_side, odometry_config_t &config)
-: left_side(&left_side), right_side(&right_side), imu(NULL), config(&config)
+Odometry::Odometry(vex::motor_group &left_side, vex::motor_group &right_side, odometry_config_t &config, bool is_async)
+: left_side(left_side), right_side(right_side), imu(NULL), config(config)
 {
-    // Create a raw pointer containing a list of all the hardware pointers needed to run the thread.
-    // This is done because callbacks do not allow class member functions
-    void* list[] = {this->left_side, this->right_side, this->imu, this->config, &mut, &current_pos};
+    // Make sure the last known info starts zeroed
+    memset(&stored_info, 0, sizeof(stored_info_t));
 
-    // Initialize and start the background task
-    handle = new vex::task(background_task, list);
+    // Start the asynchronous background thread
+    if (is_async)
+        handle = new vex::task(background_task, this);
  
 }
 
 /**
  * The background task constantly polling the motors and updating the position.
  */
-int background_task(void* obj_ptrs)
+int background_task(void* odom_obj)
 {
-    // Parse the hardware passed into the function
-    void** list = (void**) obj_ptrs;
-
-    // Dereference the pointers and store as object references to make it cleaner later
-    vex::motor_group &left_side = *((vex::motor_group*) list[0]);
-    vex::motor_group &right_side = *((vex::motor_group*) list[1]);
-    vex::inertial *imu = (vex::inertial*) list[2]; // (keep as pointer to check for NULL)
-    odometry_config_t &config = *((odometry_config_t*) list[3]);
-    vex::mutex &mut = *((vex::mutex*) list[4]);
-    position_t &current_pos = *((position_t*) list[5]);
-
-    double last_lside = 0, last_rside = 0;
-
-    while(true)
+    Odometry &odom = *((Odometry*) odom_obj);
+    while(!odom.end_task)
     {
-        double angle = 0;
-
-        // If the IMU was passed in, use it for rotational data
-        if(imu != NULL)
-            angle = imu->heading();
-        else
-        {
-            // Get the difference in distance driven between the two sides
-            double distance_diff = (left_side.position(rotationUnits::rev) - right_side.position(rotationUnits::rev)) * PI * config.wheel_diam;
-
-            //Use the arclength formula to calculate the angle.
-            angle = (180.0 / PI) * (distance_diff / config.dist_between_wheels);
-            
-            //Limit the angle betwen 0 and 360. 
-            //fmod (floating-point modulo) gets it between -359 and +359, so tack on another 360 if it's negative.
-            angle = fmod(angle, 360.0);
-            if(angle < 0)
-                angle += 360;
-        }
-
-        angle *= PI / 180.0; // Degrees to radians
-
-        // Get the encoder values in revolutions (Make sure the gearsetting is set)
-        double curr_lside = left_side.position(rotationUnits::rev);
-        double curr_rside = right_side.position(rotationUnits::rev);
-
-        // Convert the revolutions into "change in distance", and average the values for a "distance driven"
-        double lside_diff = (curr_lside - last_lside) * config.wheel_diam;
-        double rside_diff = (curr_rside - last_rside) * config.wheel_diam;
-        double dist_driven = (lside_diff + rside_diff) / 2.0;
-
-        // Create a vector from the change in distance in the current direction of the robot
-        Vector chg_vec(angle, dist_driven);
-        
-        // Create a vector from the current position in reference to X,Y=0,0
-        Vector::point_t curr_point = {.x = current_pos.x, .y = current_pos.y};
-        Vector curr_vec(curr_point);
-
-        // Tack on the "difference" vector to the current vector
-        Vector new_vec = curr_vec + chg_vec;
-
-        // Store the left and right encoder values to find the difference in the next iteration
-        last_lside = curr_lside;
-        last_rside = curr_rside;
-
-        // Avoid race conditions by locking the mutex
-        mut.lock();
-
-        // Get the X and Y coordinates from the vector and save them to the "current position"
-        current_pos.x = new_vec.get_x();
-        current_pos.y = new_vec.get_y();
-        current_pos.rot = angle;
-
-        mut.unlock();
+        odom.update();
 
         vexDelay(DOWNTIME);
     }
+
+    return 0;
+}
+
+/**
+ * End the background task. Cannot be restarted.
+ * If the user wants to end the thread but keep the data up to date,
+ * they must run the update() function manually from then on.
+ */
+void Odometry::end_async()
+{
+    this->end_task = true;
+}
+
+/**
+ * Update, store and return the current position of the robot. Only use if not initializing
+ * with a separate thread.
+ */
+position_t Odometry::update()
+{
+    position_t updated_pos;
+
+    double lside_revs = left_side.position(rotationUnits::rev);
+    double rside_revs = right_side.position(rotationUnits::rev);
+
+    if(imu == NULL)
+    {
+        updated_pos = calculate_new_pos(config, stored_info, lside_revs, rside_revs);
+    } else
+    {
+        double angle = imu->rotation(rotationUnits::deg);
+        updated_pos = calculate_new_pos(config, stored_info, lside_revs, rside_revs, &angle);
+    }
+    
+    // Update the class' stored position
+    mut.lock();
+    this->current_pos = updated_pos;
+    mut.unlock();
+
+    return updated_pos;
 }
 
 /**
@@ -121,6 +96,7 @@ position_t Odometry::get_position()
 {
     mut.lock();
 
+    // Create a new struct to pass-by-value
     position_t out = 
     {
         .x = current_pos.x,
@@ -165,3 +141,61 @@ double Odometry::rot_diff(position_t &pos1, position_t &pos2)
     return pos2.rot - pos1.rot;
 }
 
+/**
+ * Using information about the robot's mechanical structure and sensors, calculate a new position
+ * of the robot, relative to when this method was previously ran.
+ */
+position_t Odometry::calculate_new_pos(odometry_config_t &config, stored_info_t &stored_info, double lside_revs, double rside_revs, double *gyro_angle_deg)
+{
+    position_t new_pos;
+
+    double angle = 0;
+
+        // If the IMU data was passed in, use it for rotational data
+        if(gyro_angle_deg != NULL)
+            angle = *gyro_angle_deg;
+        else
+        {
+            // Get the difference in distance driven between the two sides
+            // Uses the absolute position of the encoders, so resetting them will result in
+            // a bad angle.
+            double distance_diff = (lside_revs - rside_revs) * PI * config.wheel_diam;
+
+            //Use the arclength formula to calculate the angle.
+            angle = (180.0 / PI) * (distance_diff / config.dist_between_wheels);
+            
+            //Limit the angle betwen 0 and 360. 
+            //fmod (floating-point modulo) gets it between -359 and +359, so tack on another 360 if it's negative.
+            angle = fmod(angle, 360.0);
+            if(angle < 0)
+                angle += 360;
+        }
+
+        angle *= PI / 180.0; // Degrees to radians
+
+        // Convert the revolutions into "change in distance", and average the values for a "distance driven"
+        double lside_diff = (lside_revs - stored_info.lside) * PI * config.wheel_diam;
+        double rside_diff = (rside_revs - stored_info.rside) * PI * config.wheel_diam;
+        double dist_driven = (lside_diff + rside_diff) / 2.0;
+
+        // Create a vector from the change in distance in the current direction of the robot
+        Vector chg_vec(angle, dist_driven);
+        
+        // Create a vector from the current position in reference to X,Y=0,0
+        Vector::point_t curr_point = {.x = stored_info.pos.x, .y = stored_info.pos.y};
+        Vector curr_vec(curr_point);
+
+        // Tack on the "difference" vector to the current vector
+        Vector new_vec = curr_vec + chg_vec;
+
+        new_pos.x = new_vec.get_x();
+        new_pos.y = new_vec.get_y();
+        new_pos.rot = angle * (180.0 / PI);
+
+        // Store the left and right encoder values to find the difference in the next iteration
+        stored_info.lside = lside_revs;
+        stored_info.rside = rside_revs;
+        stored_info.pos = new_pos;
+
+    return new_pos;
+}
