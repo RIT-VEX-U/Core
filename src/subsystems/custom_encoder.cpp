9d59bbe15bcf0fa56966fa6b53d276b3e859cfac
Dana <dec1701@rit.edu>
2022-03-29 22:19:04 -0400

    config updated

--- src/subsystems/custom_encoder.cpp
+++ src/subsystems/custom_encoder.cpp
@@ -3,30 +3,31 @@
 CustomEncoder::CustomEncoder(vex::triport::port &port, double ticks_per_rev)
 : super(port)
 {
-  tick_scalar = 90 / ticks_per_rev;
+  // bc it's a quadrature encoder, ticks per rev has to be multiplied by 4
+  tick_scalar = 360 / (ticks_per_rev * 4);
 }
 
 void CustomEncoder::setRotation(double val, vex::rotationUnits units)
 {
-  super::setRotation(val * tick_scalar, units);
+  super::setRotation(val / tick_scalar, units);
 }
 
 void CustomEncoder::setPosition(double val, vex::rotationUnits units)
 {
-  super::setPosition(val * tick_scalar, units);
+  super::setPosition(val / tick_scalar, units);
 }
 
 double CustomEncoder::rotation(vex::rotationUnits units)
 {
-  return super::rotation(units) / tick_scalar;
+  return super::rotation(units) * tick_scalar;
 }
 
 double CustomEncoder::position(vex::rotationUnits units)
 {
-  return super::position(units) / tick_scalar;
+  return super::position(units) * tick_scalar;
 }
 
 double CustomEncoder::velocity(vex::velocityUnits units)
 {
-  return super::velocity(units) / tick_scalar;
+  return super::velocity(units) * tick_scalar;
 }
