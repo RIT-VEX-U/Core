4e4420807b8a160a54bb64c46e8832d779204982
Ryan McGee <ramtech51@comcast.net>
2022-09-18 17:34:40 -0400

    Added FeedForward class
    from 2021-2022-BIG/motion_profile_dev

---
+++ include/utils/feedforward.h
@@ -0,0 +1,61 @@
+#pragma once
+
+/**
+ * FeedForward
+ * 
+ * Stores the feedfoward constants, and allows for quick computation.
+ * Feedfoward should be used in systems that require smooth precise movements
+ * and have high inertia, such as drivetrains and lifts.
+ * 
+ * This is best used alongside a PID loop, with the form:
+ * output = pid.get() + feedforward.calculate(v, a);
+ * 
+ * In this case, the feedforward does the majority of the heavy lifting, and the 
+ * pid loop only corrects for inconsistencies
+ * 
+ * For information about tuning feedforward, I reccommend looking at this post:
+ * https://www.chiefdelphi.com/t/paper-frc-drivetrain-characterization/160915
+ * (yes I know it's for FRC but trust me, it's useful)
+ * 
+ * @author Ryan McGee
+ * @date 6/13/2022
+ */
+class FeedForward
+{
+    public:
+
+    /**
+     * kS - Coefficient to overcome static friction: the point at which the motor *starts* to move.
+     * kV - Veclocity coefficient: the power required to keep the mechanism in motion. Multiplied by the requested velocity.
+     * kA - Acceleration coefficient: the power required to change the mechanism's speed. Multiplied by the requested acceleration.
+     * kG - Gravity coefficient: only needed for lifts. The power required to overcome gravity and stay at steady state.
+     *      Should relate to acceleration due to gravity and mass of the lift.
+     */
+    typedef struct
+    {
+        double kS, kV, kA, kG;
+    } ff_config_t;
+
+    
+    FeedForward(ff_config_t &cfg) : cfg(cfg) {}
+
+    /**
+     * @brief Perform the feedforward calculation
+     * 
+     * This calculation is the equation:
+     * F = kG + kS*sgn(v) + kV*v + kA*a
+     * 
+     * @param v Requested velocity of system
+     * @param a Requested acceleration of system
+     * @return A feedforward that should closely represent the system if tuned correctly
+     */
+    double calculate(double v, double a)
+    {
+        return (cfg.kS * (v > 0 ? 1 : v < 0 ? -1 : 0)) + (cfg.kV * v) + (cfg.kA * a) + cfg.kG;
+    }
+
+    private:
+
+    ff_config_t &cfg;
+
+};
