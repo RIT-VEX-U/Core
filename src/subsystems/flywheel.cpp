4280e403b51b7e94cc73b12b5f1c3658794fd4bf
Christopher Nokes <crn2267@rit.edu>
2022-09-18 15:57:40 -0400

    Added async functionality to Flywheel.

--- src/subsystems/flywheel.cpp
+++ src/subsystems/flywheel.cpp
@@ -8,11 +8,13 @@
 * EDIT HISTORY
 **********************************************************
 * 09/16/2022  <CRN> Created file, added constructor, spins, RPM setting, stop.
+* 09/18/2022  <CRN> Added async functionality.
 *********************************************************/
 
 #include "../core/include/subsystems/flywheel.h"
 #include "../core/include/utils/pid.h"
 #include "vex.h"
+#include <thread>
 
 using namespace vex;
 
@@ -25,32 +27,45 @@ Flywheel::Flywheel(motor_group &motors, robot_specs_t &config, PID &pid)
 // Spin motors using voltage; defaults forward at 12 volts
 // Speed is between -1 and 1.
 void Flywheel::spin(double speed, directionType dir){
-  motors.spin(dir, speed * VOLTAGECONSTANT, voltageUnits::volt);
+  motors.spin(dir, speed * 12, voltageUnits::volt);
 }
 
-
-// Gets to RPM; continue calling at some delay
-bool Flywheel::spinRPM(int RPM) {
-  // run if this is the first time calling
-  if(!inRPMLoop){
-    pid.set_target(RPM);
-    inRPMLoop = true;
+// spin this flywheel at a given RPM, async; runs until stop(), stopThread(), or a new spinRPM() is called. 
+int spinRPMThread(void* wheelPointer) {
+  Flywheel* wheel = (Flywheel*) wheelPointer;
+  wheel->getPID()->set_target((double) wheel->getRPM());  // get the pid from the wheel and set its target to the RPM stored in the wheel.
+  while(true) {
+    wheel->getPID()->update(wheel->getMotors()->velocity(velocityUnits::rpm));        // check the current velocity and update the PID with it.
+    wheel->getMotors()->spin(fwd, wheel->getPID()->get() * 12, voltageUnits::volt);   // set the motors to whatever PID tells them to do
+    vexDelay(20);
   }
-
-  pid.update(motors.velocity(velocityUnits::rpm));
-
-  if(pid.is_on_target()) {
-    inRPMLoop = false;
-    return true;
-  } return false;
+  return 0; // only here to make the compiler SHUT UP
 }
 
-// stop
+// starts / restarts RPM thread at new value
+void Flywheel::spinRPM(int inputRPM) {
+  RPM = inputRPM;
+  rpmThread.interrupt();
+  rpmThread = thread(spinRPMThread, this);
+}
+
+// return the current value that the RPM should be set to;
+double Flywheel::getRPM() { return RPM; }
+
+// Returns a POINTER TO the PID; only for use in the spinRPMThread function.
+PID* Flywheel::getPID() { return &pid; }
+
+// Returns a POINTER TO the motors; only for use in the spinRPMThread function.
+motor_group* Flywheel::getMotors() { return &motors; }
+
+// stop the RPM thread and the wheel
 void Flywheel::stop() {
+  rpmThread.interrupt();
   motors.stop();
 }
 
-// Sets RPM based on distance to lubber's hole
-void Flywheel::spinToDistance(double distance){
-  
+
+// end the thread but keep spinning the wheel; not sure why anyone would use this but here it is anyway.
+void Flywheel::stopThread() {
+  rpmThread.interrupt();
 }
