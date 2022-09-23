f7a5d7519599583d7efde53e0f4fce1e26582a97
Christopher Nokes <crn2267@rit.edu>
2022-09-22 20:11:38 -0400

    Added documentation, fixed async stuff.

--- src/subsystems/flywheel.cpp
+++ src/subsystems/flywheel.cpp
@@ -9,6 +9,7 @@
 **********************************************************
 * 09/16/2022  <CRN> Created file, added constructor, spins, RPM setting, stop.
 * 09/18/2022  <CRN> Added async functionality.
+* 09/22/2022  <CRN> Documentation improvements, fixed error if RPM is set but motor is stopped.
 *********************************************************/
 
 #include "../core/include/subsystems/flywheel.h"
@@ -19,17 +20,23 @@ using namespace vex;
 
 /*
 * Create the Flywheel object.
+* @param motors - pointer to the motors on the fly wheel
+* @param pid - pointer to the PID
 */
 Flywheel::Flywheel(motor_group &motors, PID &pid)
     :motors(motors), pid(pid){ }
 
-// Spin motors using voltage; defaults forward at 12 volts
-// Speed is between -1 and 1.
+/* Spin motors using voltage; defaults forward at 12 volts
+* @param speed - speed (between -1 and 1) to set the motor
+* @param dir - direction that the motor moves in; defaults to forward
+*/
 void Flywheel::spin_raw(double speed, directionType dir){
   motors.spin(dir, speed * 12, voltageUnits::volt);
 }
 
-// spin this flywheel at a given RPM, async; runs until stop(), stopThread(), or a new spinRPM() is called. 
+/* spin this flywheel at a given RPM, async; runs until stop(), stopThread(), or a new spinRPM() is called. 
+* @param wheelPointer - points to the current wheel object
+*/
 int spinRPMThread(void* wheelPointer) {
   Flywheel* wheel = (Flywheel*) wheelPointer;
   wheel->getPID()->set_target((double) wheel->getRPM());  // get the pid from the wheel and set its target to the RPM stored in the wheel.
@@ -41,12 +48,15 @@ int spinRPMThread(void* wheelPointer) {
   return 0; // only here to make the compiler SHUT UP
 }
 
-// starts / restarts RPM thread at new value
+/* starts / restarts RPM thread at new value
+* @param inputRPM - set the current RPM
+*/
 void Flywheel::spinRPM(int inputRPM) {
-  if(inputRPM != RPM) {
+  if(inputRPM != RPM || !taskRunning) {
     RPM = inputRPM;
     rpmTask.stop();
     rpmTask = task(spinRPMThread, this);
+    taskRunning = true;
   }
 }
 
@@ -62,11 +72,13 @@ motor_group* Flywheel::getMotors() { return &motors; }
 // stop the RPM thread and the wheel
 void Flywheel::stop() {
   rpmTask.stop();
+  taskRunning = false;
   motors.stop();
 }
 
 
 // end the thread but keep spinning the wheel; not sure why anyone would use this but here it is anyway.
 void Flywheel::stopThread() {
+  taskRunning = false;
   rpmTask.stop();
 }
