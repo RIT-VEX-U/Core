f328c46b48891a368cf7c5664d1f214d465d99a7
cowsed <richie.sommers@outlook.com>
2022-10-07 21:06:50 -0400

    Flywheel PID + Feedforward, Feedforward. Functions and configs

--- include/subsystems/flywheel.h
+++ include/subsystems/flywheel.h
@@ -11,6 +11,7 @@
 * 09/23/2022  <CRN> Added functions elaborated on in .cpp.
 *********************************************************/
 
+#include "../core/include/utils/feedforward.h"
 #include "vex.h"
 #include "../core/include/robot_specs.h"
 #include "../core/include/utils/pid.h"
@@ -21,13 +22,15 @@ class Flywheel{
   public:
 
   // CONSTRUCTORS, GETTERS, AND SETTERS
-  Flywheel(motor_group &motors, PID::pid_config_t &pid_config, const double ratio);  // constructor
+  Flywheel(motor_group &motors, PID::pid_config_t &pid_config, FeedForward::ff_config_t &ff_config, const double ratio);  // constructor
   double getRPM();                                              // returns the desired RPM
   bool isTaskRunning();                                         // returns if a task is running
   motor_group* getMotors();                                     // returns a pointer to the motors
   double getVelocity_RPM();                                     // get the current velocity of the motors in RPM
   PID* getPID();                                                // returns a pointer to the PID
   double getPIDValue();                                         // get the current OUT value of the PID
+  double getFeedforwardValue();                                 // get the current OUT value of the feedforward
+
   void setPIDTarget(double value);                              // set the PID target
   void updatePID(double value);                                 // update the PID with the current value it's tracking
 
@@ -44,6 +47,7 @@ class Flywheel{
   motor_group &motors;      // motors that make up the flywheel
   bool taskRunning = false; // is the task (thread but not) currently running?
   PID pid;                  // PID on the flywheel
+  FeedForward ff;
   double ratio;             // multiplies the velocity by this value
   double RPM = -1.0;        // Desired RPM of the flywheel. 
   task rpmTask;             // task (thread but not) that handles spinning the wheel at a given RPM
