d4ec1009d77299299ff11788c5c5e6ba3c098cb0
cowsed <richie.sommers@outlook.com>
2022-10-13 23:14:06 -0400

    Created multiple ways to construct a Flywheel each corresponding to a different control method Flywheel now chooses which spinRPMtask to use based on this

--- include/subsystems/flywheel.h
+++ include/subsystems/flywheel.h
@@ -19,10 +19,21 @@
 using namespace vex;
 
 class Flywheel{
+  enum FlywheelControlStyle{
+    PID_Feedforward,
+    Feedforward,
+    Take_Back_Half,
+    Bang_Bang,
+  };
   public:
 
   // CONSTRUCTORS, GETTERS, AND SETTERS
-  Flywheel(motor_group &motors, PID::pid_config_t &pid_config, FeedForward::ff_config_t &ff_config, const double ratio);  // constructor
+  Flywheel(motor_group &motors, PID::pid_config_t &pid_config, FeedForward::ff_config_t &ff_config, const double ratio);  // constructor for feedforward + pid
+  Flywheel(motor_group &motors, FeedForward::ff_config_t &ff_config, const double ratio);  // constructor for only feed forward
+  Flywheel(motor_group &motors, double tbh_gain, const double ratio);  // constructor for take back half
+  Flywheel(motor_group &motors, const double ratio);  // constructor for bang-bang control
+
+
   double getDesiredRPM();                                        // returns the desired RPM
   bool isTaskRunning();                                         // returns if a task is running
   motor_group* getMotors();                                     // returns a pointer to the motors
@@ -30,7 +41,8 @@ class Flywheel{
   PID* getPID();                                                // returns a pointer to the PID
   double getPIDValue();                                         // get the current OUT value of the PID
   double getFeedforwardValue();                                 // get the current OUT value of the feedforward
-
+  double getTBHGain();                                          // get the gain used for TBH control
+  
   void setPIDTarget(double value);                              // set the PID target
   void updatePID(double value);                                 // update the PID with the current value it's tracking
 
@@ -44,11 +56,14 @@ class Flywheel{
 
   private:
 
-  motor_group &motors;      // motors that make up the flywheel
-  bool taskRunning = false; // is the task (thread but not) currently running?
-  PID pid;                  // PID on the flywheel
-  FeedForward ff;
-  double ratio;             // multiplies the velocity by this value
-  double RPM = -1.0;        // Desired RPM of the flywheel. 
-  task rpmTask;             // task (thread but not) that handles spinning the wheel at a given RPM
+  motor_group &motors;                // motors that make up the flywheel
+  bool taskRunning = false;           // is the task (thread but not) currently running?
+  PID pid;                            // PID on the flywheel
+  FeedForward ff;                     // FF constants for the flywheel
+  double TBH_gain;                    // TBH gain parameter for the flywheel
+  double ratio;                       // multiplies the velocity by this value
+  double RPM = -1.0;                  // Desired RPM of the flywheel. 
+  task rpmTask;                       // task (thread but not) that handles spinning the wheel at a given RPM
+  FlywheelControlStyle control_style; // how the flywheel should be controlled
+
 };
