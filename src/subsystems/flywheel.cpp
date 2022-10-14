42252ce3eba720017a71e26e4ddd28f724a9d2ad
cowsed <richie.sommers@outlook.com>
2022-10-13 21:31:48 -0400

    Take Back Half control and tuning. Right now, the tbh gain parameter is hardcoded into the flywheel definition, this should be extracted so we can use the same code with different configs for different flywheels.

--- src/subsystems/flywheel.cpp
+++ src/subsystems/flywheel.cpp
@@ -16,6 +16,7 @@
 
 #include "../core/include/subsystems/flywheel.h"
 #include "../core/include/utils/pid.h"
+#include "../core/include/utils/math_util.h"
 #include "vex.h"
 
 using namespace vex;
@@ -27,14 +28,15 @@ using namespace vex;
 /*
 * Create the Flywheel object.
 * @param motors - pointer to the motors on the fly wheel
-* @param pid - pointer to the PID
+* @param pid - pointer to the PID config
+* @param ff  - pointer to the feedforward config
 * @param ratio - ratio of the whatever just multiplies the velocity
 */
-Flywheel::Flywheel(motor_group &motors, PID::pid_config_t &pid_config, const double ratio)
-    :motors(motors), pid(pid_config), ratio(ratio) { }
+Flywheel::Flywheel(motor_group &motors, PID::pid_config_t &pid_config, FeedForward::ff_config_t &ff_config, const double ratio)
+    :motors(motors), pid(pid_config), ff(ff_config), ratio(ratio) { }
 
 // return the current value that the RPM should be set to;
-double Flywheel::getRPM() { return RPM; }
+double Flywheel::getDesiredRPM() { return RPM; }
 
 // returns whether or not the wheel is running a task currently
 bool Flywheel::isTaskRunning() { return taskRunning; }
@@ -43,13 +45,20 @@ bool Flywheel::isTaskRunning() { return taskRunning; }
 motor_group* Flywheel::getMotors() { return &motors; } // TODO -- Remove?
 
 // return the current velocity of the flywheel motors, in RPM
-double Flywheel::getVelocity_RPM() { return ratio * motors.velocity(velocityUnits::rpm); }
+double Flywheel::getRPM() { return ratio * motors.velocity(velocityUnits::rpm); }
 
 // Returns a POINTER TO the PID; not currently used.
 PID* Flywheel::getPID() { return &pid; } // TODO -- Remove?
 
 // returns the current OUT value of the PID
 double Flywheel::getPIDValue() { return pid.get(); }
+// returns the current OUT value of the Feedforward
+// uses the current set rpm and the acceleration by ?????????
+double Flywheel::getFeedforwardValue() { 
+  double v = getDesiredRPM();
+  
+  return ff.calculate(v, 0); 
+}
 
 /* 
 * Sets the value of the PID target
@@ -70,24 +79,12 @@ void Flywheel::updatePID(double value) { pid.update(value); }
 * @param wheelPointer - points to the current wheel object
 *********************************************************/
 
-// Runs a PID loop to get to the set RPM.
-int spinRPMTask_PID(void* wheelPointer) {
-  Flywheel* wheel = (Flywheel*) wheelPointer;
-  // get the pid from the wheel and set its target to the RPM stored in the wheel.
-  while(true) {
-    wheel->updatePID(wheel->getVelocity_RPM());   // check the current velocity and update the PID with it.
-    wheel->spin_raw(wheel->getPIDValue(), fwd);   // set the motors to whatever PID tells them to do
-    vexDelay(1);
-  }
-  return 0; // only here to make the compiler SHUT UP
-}
-
 //  Runs a BANG BANG variant. I have NO IDEA if it will work properly but I have NOTHING to test this on.
 int spinRPMTask_BangBang(void* wheelPointer) {
   Flywheel* wheel = (Flywheel*) wheelPointer; 
   while(true) {
     // if it below the RPM, go, otherwise don't
-    if(wheel->getVelocity_RPM() < wheel->getRPM()) { 
+    if(wheel->getRPM() < wheel->getDesiredRPM()) { 
       wheel->spin_raw(1, fwd);
     }   
     else { wheel->stopMotors(); }
@@ -96,14 +93,64 @@ int spinRPMTask_BangBang(void* wheelPointer) {
   return 0; // only here to make the compiler SHUT UP
 }
 
-// Runs a Feedforward variant; FUNCTION STUB
-int spinRPMTask_Feedforward(void* wheelPointer) { return 0; }
+// Runs a Feedforward variant;
+int spinRPMTask_Feedforward(void* wheelPointer) { 
+  Flywheel* wheel = (Flywheel*) wheelPointer;
+  // get the pid from the wheel and set its target to the RPM stored in the wheel.
+  while(true) {
+    wheel->updatePID(wheel->getRPM());   // check the current velocity and update the PID with it.
+    double output = wheel->getFeedforwardValue();
+    wheel->spin_raw(output, fwd);   // set the motors to whatever feedforward tells them to do
+    vexDelay(1);
+  }
+  return 0; 
+}
 
-// Runs a PID + Feedforward variant; FUNCTION STUB
-int spinRPMTask_PID_Feedforward(void* wheelPointer) { return 0; }
+// Runs a PID + Feedforward variant;
+int spinRPMTask_PID_Feedforward(void* wheelPointer) {
+  Flywheel* wheel = (Flywheel*) wheelPointer;
+  // get the pid from the wheel and set its target to the RPM stored in the wheel.
+  while(true) {
+    wheel->updatePID(wheel->getRPM());   // check the current velocity and update the PID with it.
+    double output = wheel->getPIDValue() + wheel->getFeedforwardValue();
+    wheel->spin_raw(output, fwd);   // set the motors to whatever PID tells them to do
+    vexDelay(1);
+  }
+  return 0; 
+}
 
-// Runs a Take Back Half variant; FUNCTION STUB
-int spinRPMTask_TBH(void* wheelPointer) { return 0; }
+// Runs a Take Back Half variant;
+//https://www.vexwiki.org/programming/controls_algorithms/tbh
+int spinRPMTask_TBH(void* wheelPointer) {
+  Flywheel* wheel = (Flywheel*) wheelPointer;
+  double gain = .00001;
+
+  double tbh = 0.0;
+  double output = 0.0;
+  double previous_error = 0.0;
+
+  while (true){
+    //reset if set to 0, this keeps the tbh val from screwing us up when we start up again
+    if (wheel->getDesiredRPM()==0){
+      output = 0;
+      tbh = 0;
+    }
+   
+    double error = wheel->getDesiredRPM() - wheel->getRPM();
+    output += gain * error;
+    wheel->spin_raw(clamp(output, 0, 1), fwd);
+
+    if (sign(error)!=sign(previous_error)){
+      output = .5 * (output + tbh);
+      tbh = output;
+      previous_error = error;
+    }
+
+    vexDelay(1);
+  }
+  
+   return 0; 
+}
 
 // Runs a 'Moving average filter with above closed loop systems' variant, whatever that means; FUNCTION STUB
 int spinRPMTask_ClosedLoop(void* wheelPointer) { return 0; }
@@ -133,7 +180,7 @@ void Flywheel::spin_manual(double speed, directionType dir){
 void Flywheel::spinRPM(int inputRPM) {
   // only run if the RPM is different or it isn't already running
   if(!taskRunning) {
-    rpmTask = task(spinRPMTask_PID, this);
+    rpmTask = task(spinRPMTask_TBH, this);
     taskRunning = true;
   }
   RPM = inputRPM;
