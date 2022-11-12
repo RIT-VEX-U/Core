7a525038a844caef44c4635ab85b6bc58904f567
Dana <dec1701@rit.edu>
2022-11-11 23:17:09 -0500

    flywheel commands

---
+++ include/utils/command_structure/flywheel_commands.h
@@ -0,0 +1,112 @@
+/**
+ * File: flywheel_commands.h
+ * Desc:
+ *    [insert meaningful desc]
+ */
+
+#pragma once
+
+#include "../core/include/subsystems/flywheel.h"
+#include "../core/include/utils/command_structure/auto_command.h"
+
+class SpinRawCommand: public AutoCommand {
+  public:
+    SpinRawCommand(Flywheel &flywheel, double speed, directionType dir=fwd);
+
+    /**
+     * Run spin_raw
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+
+    // parameters for spin_raw
+    double speed;
+    directionType dir;
+};
+
+class SpinManualCommand: public AutoCommand {
+  SpinManualCommand(Flywheel &flywheel, double speed, directionType dir=fwd);
+
+    /**
+     * Run spin_manual
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+
+    // parameters for spin_manual
+    double speed;
+    directionType dir;
+};
+
+class SpinRPMCommand: AutoCommand {
+  SpinRPMCommand(Flywheel &flywheel, int rpm);
+
+    /**
+     * Run spin_manual
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+
+    // parameters for spinRPM
+    int rpm;
+};
+
+class FlywheelStopCommand: public AutoCommand {
+  FlywheelStopCommand(Flywheel &flywheel);
+
+    /**
+     * Run stop
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+};
+
+class FlywheelStopMotorsCommand: public AutoCommand {
+  FlywheelStopMotorsCommand(Flywheel &flywheel);
+
+    /**
+     * Run stop
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+};
+
+class FlywheelStopNonTasksCommand: public AutoCommand {
+  FlywheelStopNonTasksCommand(Flywheel &flywheel);
+
+    /**
+     * Run stop
+     * Overrides run from AutoCommand
+     * @returns true when execution is complete, false otherwise
+     */
+    bool run() override;
+
+  private:
+    // Flywheel instance to run the function on
+    Flywheel &flywheel;
+};
