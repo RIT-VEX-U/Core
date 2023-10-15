0f39b814e9e505a9a2a293b677c66b48e5bae6fe
Ryan McGee <ramtech51@comcast.net>
2023-10-15 13:43:27 -0400

    Started OdometryFusion

--- include/subsystems/odometry/odometry_fusion.h
+++ include/subsystems/odometry/odometry_fusion.h
@@ -2,18 +2,29 @@
 #include "../core/include/subsystems/odometry/odometry_base.h"
 #include "../core/include/subsystems/custom_encoder.h"
 
+typedef struct 
+{
+    int movingavg_buffersize;
+} odomfus_cfg_t;
+
 class OdometryFusion : OdometryBase
 {
 public:
 
-    OdometryFusion(vex::motor_group &left_side, vex::motor_group &right_side, robot_specs_t &config, vex::inertial imu, bool is_async = true);
-    OdometryFusion(CustomEncoder &left_custom_enc, CustomEncoder &right_custom_enc, robot_specs_t &config, vex::inertial imu, bool is_async = true);
+    OdometryFusion(vex::motor_group &left_side, vex::motor_group &right_side, robot_specs_t &config, vex::inertial &imu, bool is_async = true);
+    OdometryFusion(CustomEncoder &left_custom_enc, CustomEncoder &right_custom_enc, robot_specs_t &config, vex::inertial &imu, bool is_async = true);
 
     pose_t update() override;
 
 
 private:
 
-    static pose_t calculate_new_pose();
+    pose_t calculate_new_pose(robot_specs_t &config, pose_t &stored_info, double lside_rev, double rside_rev, double side_accel, double angle_deg);
+
+    vex::motor_group *left_side, *right_side;
+    CustomEncoder *left_custom_enc, *right_custom_enc;
+    robot_specs_t &config;
+    
+    vex::inertial &imu;
     
 };
--- src/subsystems/odometry/odometry_fusion.cpp
+++ src/subsystems/odometry/odometry_fusion.cpp
@@ -1,13 +1,16 @@
 #include "../core/include/subsystems/odometry/odometry_fusion.h"
+#include "../core/include/utils/vector2d.h"
 
-OdometryFusion::OdometryFusion(vex::motor_group &left_side, vex::motor_group &right_side, robot_specs_t &config, vex::inertial imu, bool is_async = true)
-: OdometryTank(left_side, right_side, config, &imu, is_async)
+
+
+OdometryFusion::OdometryFusion(vex::motor_group &left_side, vex::motor_group &right_side, robot_specs_t &config, vex::inertial &imu, bool is_async)
+: left_side(&left_side), right_side(&right_side), config(config), imu(imu), OdometryBase(is_async)
 {
 
 }
 
-OdometryFusion::OdometryFusion(CustomEncoder &left_custom_enc, CustomEncoder &right_custom_enc, robot_specs_t &config, vex::inertial imu, bool is_async = true)
-: OdometryTank(left_custom_enc, right_custom_enc, config, &imu, is_async)
+OdometryFusion::OdometryFusion(CustomEncoder &left_custom_enc, CustomEncoder &right_custom_enc, robot_specs_t &config, vex::inertial &imu, bool is_async)
+: left_custom_enc(&left_custom_enc), right_custom_enc(&right_custom_enc), config(config), imu(imu), OdometryBase(is_async)
 {
 
 }
@@ -15,7 +18,23 @@ OdometryFusion::OdometryFusion(CustomEncoder &left_custom_enc, CustomEncoder &ri
 /**
  * Get information from the input hardware and an existing position, and calculate a new current position
  */
-pose_t OdometryFusion::calculate_new_pos(robot_specs_t &config, pose_t &stored_info, double lside_diff, double rside_diff, double angle_deg)
+pose_t OdometryFusion::calculate_new_pose(robot_specs_t &config, pose_t &stored_info, double lside_rev, double rside_rev, double side_accel, double angle_deg)
 {
+    // Store the encoder readings for future use
+    static double last_lside_rev = lside_rev;
+    static double last_rside_rev = rside_rev;
+    
+    double lside_delta_rev = lside_rev - last_lside_rev;
+    double rside_delta_rev = rside_rev - last_rside_rev;
+
+    double avg_delta_rev = (lside_delta_rev + rside_delta_rev) / 2.0;
+    double avg_delta_dist = avg_delta_rev * PI * config.odom_wheel_diam * config.odom_gear_ratio;
+    
+    // Perform a double integral on the acceleration
+    static vex::timer tmr;
+    static uint32_t last_time_ms = tmr.time();
+    static double last_side_accel = side_accel;
+
+
     
 }
