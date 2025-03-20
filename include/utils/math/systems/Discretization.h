80adbf515ed2f45211a09cdf7696392bed81563b
PascalSkylake <jackcammarata1@gmail.com>
2025-03-20 16:31:34 -0400

    make linear system and exact discretization of A and B

---
+++ include/utils/math/systems/Discretization.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../core/include/utils/math/eigen_interface.h"
+#include "../vendor/eigen/unsupported/Eigen/MatrixFunctions"
+
+#include <tuple>
+
+/**
+ * Discretizes the continuous system and input matrices (A and B) over the
+ * timestep dt in seconds.
+ * 
+ * @tparam STATES Dimension of the state matrix.
+ * @tparam INPUTS Dimension of the input matrix.
+ * 
+ * @param Ac The continuous state matrix A.
+ * @param Bc The continuous input matrix B.
+ * @param dt The timestep in seconds.
+ */
+template <int STATES, int INPUTS>
+std::tuple<EMat<STATES, STATES>, EMat<STATES, INPUTS>> discretize_AB(EMat<STATES, STATES> Ac, EMat<STATES, INPUTS> Bc, const double &dt) {
+    // Form the intermediate matrix U
+    //
+    //       [A B]
+    //   U = [0 0]
+    //
+    EMat<2 * STATES, STATES + INPUTS> U;
+    U.template block<STATES, STATES>(0, 0) = Ac;
+    U.template block<STATES, INPUTS>(0, STATES) = Bc;
+    U.template block<STATES, STATES + INPUTS>(STATES, 0).setZero();
+
+    //
+    //  [A B]
+    //  [0 0] * T     [Ad Bd]
+    // e           =  [0   I]
+    //
+    EMat<2 * STATES, STATES + INPUTS> phi = (U * dt).exp();
+
+    // Extract Ad and Bd from phi and put them in a tuple
+    return std::make_tuple(phi.template block<STATES, STATES>(0, 0), phi.template block<STATES, INPUTS>(0, STATES));
+}
