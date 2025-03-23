30019bac4cc7d22fb5d515185ff07cbb7ed3145c
PascalSkylake <jackcammarata1@gmail.com>
2025-03-23 15:19:45 -0400

    everything commented

--- include/utils/controls/state_space/linear_plant_inversion.h
+++ include/utils/controls/state_space/linear_plant_inversion.h
@@ -8,66 +8,141 @@
 /**
  * This class computes a feedforward control input by inverting the discrete
  * plant dynamics. A continuous linear system is provided, it is then
- * discretized with some timestep, then the feedforward control input
+ * discretized on some timestep, then the feedforward control input
  * is computed to satisfy:
- * 
+ *
  *   B_d * u_ff = next_state - A_d * current_state
  */
 template <int STATES, int INPUTS> class LinearPlantInversionFeedforward {
   public:
+    /**
+     * Constructs a feedforward given a plant and the nominal timestep.
+     * 
+     * @tparam OUTPUTS The number of outputs of the plant.
+     * @param plant The linear system.
+     * @param dt The nominal timestep in seconds.
+     */
     template <int OUTPUTS>
     LinearPlantInversionFeedforward(LinearSystem<STATES, INPUTS, OUTPUTS> &plant, const double &dt)
         : LinearPlantInversionFeedforward(plant.A(), plant.B(), dt) {}
 
+    /**
+     * Construts a feedforward given the state and input matrices from a plant.
+     * 
+     * @param A The state matrix of the linear system.
+     * @param B The input matrix of the linear system.
+     * @param dt The nominal timestep in seconds.
+     */
     LinearPlantInversionFeedforward(const EMat<STATES, STATES> &A, const EMat<STATES, INPUTS> &B, const double &dt)
-        : m_dt(dt) {
+        : A_(A), B_(B), m_dt(dt) {
         auto [Ad, Bd] = discretize_AB(A, B, dt);
-        m_Ad = Ad;
-        m_Bd = Bd;
+        Ad_ = Ad;
+        Bd_ = Bd;
     }
 
+    /**
+     * Computes the feedforward control input given the current reference state
+     * and the next reference state. This also sets the current reference state
+     * to the next reference state for you.
+     * 
+     * @param r The current reference state.
+     * @param next_r The next reference state.
+     */
     EVec<INPUTS> calculate(const EVec<STATES> &r, const EVec<STATES> &next_r) {
-        m_uff = m_Bd.householderQr().solve(next_r - (m_Ad * r));
-        m_r = next_r;
+        // ẋ = Ax + Bu
+        // Bu = ẋ - Ax
+        // u = B \ (ẋ - Ax)
+        // u = B \ (next_r - Br)
+        uff_ = Bd_.householderQr().solve(next_r - (Ad_ * r));
+        r_ = next_r;
 
-        return m_uff;
+        return uff_;
     }
 
-    EVec<INPUTS> calculate(const EVec<STATES> &next_r) { return calculate(m_r, next_r); }
+    /**
+     * Computes the feedforward control input given only the next reference state.
+     * This assumes that the previous reference is already set.
+     * 
+     * @param next_r The next reference state.
+     */
+    EVec<INPUTS> calculate(const EVec<STATES> &next_r) { return calculate(r_, next_r); }
 
+    /**
+     * Computes the feedforward control input given the current reference state
+     * and the next reference state. This also sets the current reference state
+     * to the next reference state for you. Use this function if your timestep
+     * is not the same between each run.
+     * 
+     * This is slower because it discretizes A and B on each run, requiring
+     * computing a matrix exponential. Don't use this unless you have to.
+     * 
+     * @param r The current reference state.
+     * @param next_r The next reference state.
+     * @param dt The timestep for this run.
+     */
     EVec<INPUTS> calculate(const EVec<STATES> &r, const EVec<STATES> &next_r, const double &dt) {
-        std::cout << "made it here" << std::endl;
-        auto [Ad, Bd] = discretize_AB(A, B, dt);
+        auto [Ad, Bd] = discretize_AB(A_, B_, dt);
 
-        m_uff = Bd.householderQr().solve(next_r - (Ad * r));
-        m_r = next_r;
+        // ẋ = Ax + Bu
+        // Bu = ẋ - Ax
+        // u = B \ (ẋ - Ax)
+        // u = B \ (next_r - Br)
+        uff_ = Bd.householderQr().solve(next_r - (Ad * r));
+        r_ = next_r;
 
-        return m_uff;
+        return uff_;
     }
 
-    EVec<INPUTS> calculate(const EVec<STATES> &next_r, const double &dt) { return calculate(m_r, next_r, dt); }
+    /**
+     * Computes the feedforward control input given only the next reference state.
+     * This assumes that the previous reference is already set.
+     * 
+     * This is slower because it discretizes A and B on each run, requiring
+     * computing a matrix exponential. Don't use this unless you have to.
+     * 
+     * @param next_r The next reference state.
+     * @param dt The timestep for this run.
+     */
+    EVec<INPUTS> calculate(const EVec<STATES> &next_r, const double &dt) { return calculate(r_, next_r, dt); }
 
+    /**
+     * Resets the reference to the given state, and the feedforward to zero.
+     * 
+     * @param initial_state The state to set the current reference to.
+     */
     void reset(const EVec<STATES> &initial_state) {
-        m_r = initial_state;
-        m_uff.setZero();
+        r_ = initial_state;
+        uff_.setZero();
     }
 
+    /**
+     * Resets the reference to all zeros, and the feedforward to zero.
+     */
     void reset() {
-        m_r.setZero();
-        m_uff.setZero();
+        r_.setZero();
+        uff_.setZero();
     }
 
-    void set_r(const EVec<STATES> &r) { m_r = r; }
+    /**
+     * Sets the current reference to a given state.
+     * 
+     * @param r The state to set the current reference to.
+     */
+    void set_r(const EVec<STATES> &r) { r_ = r; }
 
   private:
-    EMat<STATES, STATES> A;
-    EMat<STATES, INPUTS> B;
+    // The continuous system matrices
+    EMat<STATES, STATES> A_;
+    EMat<STATES, INPUTS> B_;
 
-    EMat<STATES, STATES> m_Ad;
-    EMat<STATES, INPUTS> m_Bd;
+    // The discrete system matrices discretized on the nominal timestep
+    EMat<STATES, STATES> Ad_;
+    EMat<STATES, INPUTS> Bd_;
 
-    EVec<INPUTS> m_uff;
-    EVec<STATES> m_r;
+    // The feedforward control input
+    EVec<INPUTS> uff_;
+    // The current reference state
+    EVec<STATES> r_;
 
     double m_dt;
 };
