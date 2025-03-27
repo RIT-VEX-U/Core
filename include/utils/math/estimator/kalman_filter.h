468eb50924417a2de8fe316f9a70cd2cdd8077f2
PascalSkylake <jackcammarata1@gmail.com>
2025-03-26 20:10:34 -0400

    stuff...

--- include/utils/math/estimator/kalman_filter.h
+++ include/utils/math/estimator/kalman_filter.h
@@ -17,11 +17,6 @@
  * To read more about Kalman filters read:
  * https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
  *
- * This implementation uses the square-root form of the Kalman filter. The main
- * reason for this is to ensure that the covariance matrix always remains positive
- * definite. To see the formulation this implements, read:
- * https://arxiv.org/pdf/2208.06452
- *
  * @tparam STATES Dimension of the state vector.
  * @tparam INPUTS Dimension of the control input vector.
  * @tparam OUTPUTS Dimension of the measurement vector.
@@ -42,7 +37,10 @@ template <int STATES, int INPUTS, int OUTPUTS> class KalmanFilter {
      * @param state_stddevs The standard deviations of the states.
      * @param measurement_stddevs The standard deviations of the measurements.
      */
-    KalmanFilter(LinearSystem<STATES, INPUTS, OUTPUTS> &plant, const StateVector &state_stddevs, const OutputVector &measurement_stddevs)
+    KalmanFilter(
+      LinearSystem<STATES, INPUTS, OUTPUTS> &plant, const StateVector &state_stddevs,
+      const OutputVector &measurement_stddevs
+    )
         : KalmanFilter(plant.A(), plant.B(), plant.C(), plant.D(), state_stddevs, measurement_stddevs) {}
 
     /**
@@ -59,79 +57,61 @@ template <int STATES, int INPUTS, int OUTPUTS> class KalmanFilter {
       const StateMatrix &A, const InputMatrix &B, const EMat<OUTPUTS, STATES> &C, const EMat<OUTPUTS, INPUTS> &D,
       const StateVector &state_stddevs, const OutputVector &measurement_stddevs
     ) {
-        m_A = A;
-        m_B = B;
-        m_C = C;
-        m_D = D;
+        A_ = A;
+        B_ = B;
+        C_ = C;
+        D_ = D;
 
-        m_sqrtQ = state_stddevs.asDiagonal();
-        m_sqrtR = measurement_stddevs.asDiagonal();
+        Q_ = state_stddevs.asDiagonal();
+        Q_ = Q_ * Q_.transpose();
+        R_ = measurement_stddevs.asDiagonal();
+        R_ = R_ * R_.transpose();
 
         reset();
     }
 
     /**
-     * Returns the square-root covariance matrix S.
+     * Returns the covariance matrix P.
      */
-    const StateMatrix &S() const { return m_S; }
+    StateMatrix P() const { return P_; }
 
     /**
-     * Returns one element of the square-root covariance matrix S.
-     *
-     * @param i Row of S.
-     * @param j Column of S.
-     */
-    double S(int i, int j) const { return m_S(i, j); }
-
-    /**
-     * Set the current square-root covariance matrix S.
-     *
-     * @param S The new square-root covariance matrix S.
-     */
-    void set_S(const StateMatrix &S) { m_S = S; }
-
-    /**
-     * Returns the reconstructed covariance matrix P.
-     */
-    StateMatrix P() const { return m_S.transpose() * m_S; }
-
-    /**
-     * Set the current square-root covariance matrix S to the square-root of P.
+     * Set the current covariance matrix P.
      *
      * @param P The covariance matrix P.
      */
-    void set_P(const StateMatrix &P) { m_S = P.llt().matrixU(); }
+    void set_P(const StateMatrix &P) { P_ = P; }
 
     /**
      * Returns the current state estimate x-hat.
      */
-    const StateVector &xhat() const { return m_xhat; }
+    const StateVector &xhat() const { return xhat_; }
 
     /**
      * Returns one element of the current state estimate x-hat.
      *
      * @param i Row of x-hat.
      */
-    double xhat(int i) const { return m_xhat(i); }
+    double xhat(int i) const { return xhat_(i); }
 
     /**
      * Set the current state estimate x-hat.
      */
-    void set_xhat(const StateVector &xhat) { m_xhat = xhat; }
+    void set_xhat(const StateVector &xhat) { xhat_ = xhat; }
 
     /**
      * Set one element of the current state estimate x-hat.
      *
      * @param i Row of x-hat.
      */
-    void set_xhat(int i, double value) { m_xhat(i) = value; }
+    void set_xhat(int i, double value) { xhat_(i) = value; }
 
     /**
      * Resets the filter.
      */
     void reset() {
-        m_xhat.setZero();
-        m_S.setZero();
+        xhat_.setZero();
+        P_.setZero();
     }
 
     /**
@@ -142,21 +122,14 @@ template <int STATES, int INPUTS, int OUTPUTS> class KalmanFilter {
      */
     void predict(const InputVector &u, const double &dt) {
         // Q is discrete sqrt(process noise)
-        EMat<STATES, STATES> Q = m_sqrtQ * sqrt(dt);
-        auto [A, B] = discretize_AB(m_A, m_B, dt);
+        EMat<STATES, STATES> Q = Q_ * dt;
+        auto [A, B] = discretize_AB(A_, B_, dt);
 
         // Compute prior mean
-        // equation (4)
-        m_xhat = A * m_xhat + B * u;
+        xhat_ = A * xhat_ + B * u;
 
-        // Form temporary matrix to compute prior state covariance
-        EMat<STATES * 2, STATES> S_bar;
-        S_bar.template block<STATES, STATES>(0, 0) = (m_S * A.transpose());
-        S_bar.template block<STATES, STATES>(STATES, 0) = m_sqrtQ;
-
-        // Compute prior state covariance
-        // equation (5)
-        m_S = S_bar.householderQr().matrixQR().template block<STATES, STATES>(0, 0).template triangularView<Eigen::Upper>();
+        // Compute prior covariance
+        P_ = A * P_ * A.transpose() + Q;
     }
 
     /**
@@ -165,23 +138,24 @@ template <int STATES, int INPUTS, int OUTPUTS> class KalmanFilter {
      * @param y The vector of measurements.
      * @param u The control input used in the last predict step.
      */
-    void correct(const OutputVector &y, const InputVector &u) { correct<OUTPUTS>(y, u, m_C, m_D, m_sqrtR); }
+    void correct(const OutputVector &y, const InputVector &u) { correct<OUTPUTS>(y, u, C_, D_, R_); }
 
     /**
-     * Correct the state estimate using the measurements in y, and custom standard
-     * deviations. This is useful for when the noise in the measurements vary.
+     * Correct the state estimate using the measurements in y, and custom
+     * measurement noise matrix. This is useful for when the noise in the
+     * measurements vary.
      *
      * @param y The vector of measurements.
      * @param u The control input used in the last predict step.
-     * @param sqrtR The square root measurement noise matrix to use for this step.
+     * @param R The measurement noise matrix to use for this step.
      */
-    void correct(const OutputVector &y, const InputVector &u, const EMat<OUTPUTS, OUTPUTS> &sqrtR) {
-        correct<OUTPUTS>(y, u, m_C, m_D, sqrtR);
+    void correct(const OutputVector &y, const InputVector &u, const EMat<OUTPUTS, OUTPUTS> &R) {
+        correct<OUTPUTS>(y, u, C_, D_, R);
     }
 
     /**
      * Correct the state estimate using the measurements in y, custom measurement
-     * and feedthrough matrices, and custom measurement standard deviations.
+     * and feedthrough matrices, and custom measurement measurement noise.
      * This is useful for when a different set of measurements are being applied
      * than what the plant defines.
      *
@@ -189,66 +163,53 @@ template <int STATES, int INPUTS, int OUTPUTS> class KalmanFilter {
      * @param u The control input used in the last predict step.
      * @param C The measurement matrix to use for this step.
      * @param D The feedthrough matrix to use for this step.
-     * @param sqrtR The square root measurement noise matrix to use for this step.
+     * @param R The measurement noise matrix to use for this step.
      */
     template <int ROWS>
     void correct(
       const EVec<ROWS> &y, const InputVector &u, const EMat<ROWS, STATES> &C, const EMat<ROWS, INPUTS> &D,
-      const EMat<ROWS, ROWS> &sqrtR
+      const EMat<ROWS, ROWS> &R
     ) {
-        // R is sqrt(measurement noise)
+        // Compute the innovation covariance
+        //
+        //   Py = CPCᵀ + R
+        //
+        EMat<ROWS, ROWS> Py = C * P_ * C.transpose() + R;
 
-        // Compute measurement innovation
-        // equation (8)
-        EVec<ROWS> z = y - (C * m_xhat + D * u);
+        // Compute the optimal Kalamn gain
+        //
+        //   K = (Py \ CPᵀ)ᵀ
+        //
+        EMat<STATES, ROWS> K = Py.transpose().ldlt().solve(C * P_.transpose()).transpose();
 
-        // Form temporary matrix to compute innovation covariance
-        EMat<STATES + ROWS, ROWS> Sy_bar;
-        Sy_bar.template block<STATES, ROWS>(0, 0) = (m_S * C.transpose());
-        Sy_bar.template block<ROWS, ROWS>(STATES, 0) = sqrtR;
+        // Compute the posterior mean
+        //
+        //   x̂ = x̂ + K(y - (Cx̂ + Du))
+        //
+        xhat_ += K * (y - (C * xhat_ + D * u));
 
-        // Compute innovation covariance
-        // equation (9)
-        EMat<ROWS, ROWS> Sy =
-          Sy_bar.householderQr().matrixQR().template block<ROWS, ROWS>(0, 0).template triangularView<Eigen::Upper>();
-
-        // Compute Kalman gain
-        // equation (10)
-        EMat<STATES, ROWS> K =
-          (Sy.template triangularView<Eigen::Upper>().solve(
-             (Sy.transpose().template triangularView<Eigen::Lower>().solve(C)) * (m_S.transpose() * m_S)
-           )).transpose();
-
-        // Compute posterior state
-        // equation (13)
-        m_xhat = m_xhat + K * z;
-
-        // Form temporary matrix to compute posterior state covariance
-        EMat<STATES + ROWS, STATES> S_bar;
-        S_bar.template block<STATES, STATES>(0, 0) = (m_S * (EMat<STATES, STATES>::Identity() - (K * C)).transpose());
-        S_bar.template block<ROWS, STATES>(STATES, 0) = (sqrtR * K.transpose());
-
-        // Compute posterior state covariance
-        // equation (14)
-        m_S = S_bar.householderQr().matrixQR().template block<STATES, STATES>(0, 0).template triangularView<Eigen::Upper>();
+        // Compute the posterior covariance using the Joseph form update equation
+        //
+        // P = (I - KC)P(I - KC)ᵀ + KRKᵀ
+        //
+        P_ = (EMat<STATES, STATES>::Identity() - K * C) * P_ * (EMat<STATES, STATES>::Identity() - K * C).transpose() +
+             K * R * K.transpose();
     }
 
   private:
-    StateVector m_xhat;
-    StateMatrix m_S;
+    StateVector xhat_;
+    StateMatrix P_;
 
-    StateMatrix m_sqrtQ;
-    EMat<OUTPUTS, OUTPUTS> m_sqrtR;
+    StateMatrix Q_;
+    EMat<OUTPUTS, OUTPUTS> R_;
 
-    StateMatrix m_A;
-    EMat<STATES, INPUTS> m_B;
-    EMat<OUTPUTS, STATES> m_C;
-    EMat<OUTPUTS, INPUTS> m_D;
+    StateMatrix A_;
+    EMat<STATES, INPUTS> B_;
+    EMat<OUTPUTS, STATES> C_;
+    EMat<OUTPUTS, INPUTS> D_;
 
-    double m_dt;
+    double dt_;
 };
 
-// allow using any of these names
+// allow using both names
 template <int STATES, int INPUTS, int OUTPUTS> using KF = KalmanFilter<STATES, INPUTS, OUTPUTS>;
-template <int STATES, int INPUTS, int OUTPUTS> using SRKF = KalmanFilter<STATES, INPUTS, OUTPUTS>;
-template <int STATES, int INPUTS, int OUTPUTS> using SquareRootKalmanFilter = KalmanFilter<STATES, INPUTS, OUTPUTS>;
