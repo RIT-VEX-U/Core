24da5caae83461867f042cd76157cb25c9c075c6
cowsed <cowsed@users.noreply.github.com>
2025-04-08 23:47:41 +0000

    deploy: 244a5ba846608cfd6588e34d70a0fef96d44c1af

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['norm_0',['norm',['../classTranslation2d.html#ac311ce36aed958758a327a78337c568f',1,'Translation2d']]],
-  ['num_5fsigmas_1',['num_sigmas',['../classScaledSphericalSimplexSigmaPoints.html#a6ffa27a1ea7808a0f546a2b296b3c451',1,'ScaledSphericalSimplexSigmaPoints']]]
+  ['normalize_1',['normalize',['../classTranslation2d.html#a45c1c74c74bc0eefcd3060908e3c13e0',1,'Translation2d']]],
+  ['num_5fsigmas_2',['num_sigmas',['../classScaledSphericalSimplexSigmaPoints.html#a6ffa27a1ea7808a0f546a2b296b3c451',1,'ScaledSphericalSimplexSigmaPoints']]]
 ];
