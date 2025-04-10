ad3015dd4b58c39ba5f396c72f7cf8fa951d31f9
Jack Cammarata <50638415+PascalSkylake@users.noreply.github.com>
2025-04-10 19:03:02 -0400

    Asymmetric Motion Profiles (#110)
    Rewrites trapezoid_profile.h and trapezoid_profile.cpp to support
    different acceleration and deceleration rates
    
    here's a pdf from when I derived v_peak again to show william
    
    https://rit.enterprise.slack.com/files/U07JS7P07FZ/F08L9LL9G65/notes_250330_132755.pdf

--- include/utils/controls/trapezoid_profile.h
+++ include/utils/controls/trapezoid_profile.h
@@ -1,84 +1,76 @@
 #pragma once
 
-/**
- * motion_t is a description of 1 dimensional motion at a point in time.
- */
-typedef struct {
-  double pos;   ///< 1d position at this point in time
-  double vel;   ///< 1d velocity at this point in time
-  double accel; ///< 1d acceleration at this point in time
+#include <math.h>
 
+// Struct representing a state along the motion profile containing position, velocity and acceleration.
+typedef struct {
+  double pos; // 1D position
+  double vel; // 1D velocity
+  double acc; // 1D acceleration
 } motion_t;
 
 /**
- * Trapezoid Profile
- *
- * This is a motion profile defined by an acceleration, maximum velocity, start point and end point.
- * Using this information, a parametric function is generated, with a period of acceleration, constant
- * velocity, and deceleration. The velocity graph looks like a trapezoid, giving it it's name.
- *
- * If the maximum velocity is set high enough, this will become a S-curve profile, with only acceleration and
- * deceleration.
- *
- * This class is designed for use in properly modelling the motion of the robots to create a feedfoward
- * and target for PID. Acceleration and Maximum velocity should be measured on the robot and tuned down
- * slightly to account for battery drop.
- *
- * Here are the equations graphed for ease of understanding:
- * https://www.desmos.com/calculator/rkm3ivu1yk
- *
- * @author Ryan McGee
- * @date 7/12/2022
- *
+ * Class representing a trapezoidal motion profile. This consists of either two or three stages. First, an acceleration
+ * stage where the velocity increases to a maximum. Then a cruise stage where the velocity is constant, then a
+ * deceleration stage where the velocity decreases to zero.
+ * 
+ * This implementation allows for different acceleration and deceleration rates.
+ * 
+ * This is best used with LQR, as it tracks both velocity and position at the same time, however it can also be used
+ * with PID and feedforward.
+ * 
+ * @author Jack Cammarata
+ * @date 3/28/2025
  */
 class TrapezoidProfile {
 public:
-  /**
-   * @brief Construct a new Trapezoid Profile object
-   *
-   * @param max_v Maximum velocity the robot can run at
-   * @param accel Maximum acceleration of the robot
-   */
-  TrapezoidProfile(double max_v, double accel);
+/**
+ * Constructs a TrapezoidProfile.
+ * 
+ * @param x_initial The initial position.
+ * @param x_target The target position.
+ * @param v_max The maximum velocity.
+ * @param accel The acceleration.
+ * @param decel The deceleration.
+ */
+  TrapezoidProfile(const double &x_initial, const double &x_target, const double &v_max, const double &accel, const double &decel);
 
   /**
-   * @brief Run the trapezoidal profile based on the time that's ellapsed
-   *
-   * @param time_s Time since start of movement
-   * @return motion_t Position, velocity and acceleration
+   * Calculate the state along the motion profile at some given time.
+   * 
+   * @param t The time in seconds.
+   * 
+   * @return the state.
    */
-  motion_t calculate(double time_s);
+  motion_t calculate(double t);
 
   /**
-   * set_endpts defines a start and end position
-   * @param start the starting position of the path
-   * @param end the ending position of the path
+   * Returns the total time that the motion profile takes to complete.
+   * 
+   * @return the total time that the motion profile takes to complete.
    */
-  void set_endpts(double start, double end);
-
-  /**
-   * set_accel sets the acceleration this profile will use (the left and right legs of the trapezoid)
-   * @param accel the acceleration amount to use
-   */
-  void set_accel(double accel);
-
-  /**
-   * sets the maximum velocity for the profile
-   * (the height of the top of the trapezoid)
-   * @param max_v the maximum velocity the robot can travel at
-   */
-  void set_max_v(double max_v);
-
-  /**
-   * uses the kinematic equations to and specified accel and max_v to figure out how long moving along the profile would
-   * take
-   * @return the time the path will take to travel
-   */
-  double get_movement_time();
+  double total_time();
 
 private:
-  double start, end; ///< the start and ending position of the profile
-  double max_v;      ///< the maximum velocity to travel at for this profile
-  double accel;      ///< the rate of acceleration to use for this profile.
-  double time;       ///< the current point in time along the path
+  double x_initial_;
+  double x_target_;
+  double v_max_;
+  double v_peak_;
+  double accel_;
+  double decel_;
+  double distance_;
+  double dist_accel_;
+  double dist_decel_;
+  double dist_cruise_;
+  double dist_full_;
+
+  double time_accel_;
+  double time_decel_;
+  double time_cruise_;
+  double time_total_;
+
+  bool triangular_;
+
+  // 1 if positive, -1 if negative
+  int direction_;
 };
