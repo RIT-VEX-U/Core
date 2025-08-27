91b393dde47a96bf7947369fa2d03160a1c28234
CodingPotatoes <CodingPotatoes@users.noreply.github.com>
2025-08-27 22:00:14 +0000

    deploy: c57410766583ba59c7e984dc8d92b5b36d21f0ad

---
+++ classVDP_1_1PIDControlRecord.js
@@ -0,0 +1,5 @@
+var classVDP_1_1PIDControlRecord =
+[
+    [ "PIDControlRecord", "classVDP_1_1PIDControlRecord.html#a212024f2d90dcb97b5c5c82334670df4", null ],
+    [ "response", "classVDP_1_1PIDControlRecord.html#a170d1bb79bfda63ae2c79e98290c2904", null ]
+];
--- search/all_0.js
+++ search/all_0.js
@@ -1,17 +1,18 @@
 var searchData=
 [
   ['a_0',['A',['../classLinearSystem.html#ac053302a9b2a7d22f974e25dccf9dd2e',1,'LinearSystem']]],
-  ['accel_1',['accel',['../classOdometryBase.html#a94c814dfb542dd891d84db7e74537249',1,'OdometryBase::accel'],['../structMotionController_1_1m__profile__cfg__t.html#ac4f07f3ac52d12fd820ee4d8ce847f66',1,'MotionController::m_profile_cfg_t::accel']]],
-  ['add_2',['add',['../classCommandController.html#a03fc0dd37c12ae7f43200d5588c768b8',1,'CommandController::add(std::vector&lt; AutoCommand * &gt; cmds)'],['../classCommandController.html#a4cb2c06abd92fb99578f0da75c1411e5',1,'CommandController::add(AutoCommand *cmd, double timeout_seconds=10.0)'],['../classCommandController.html#a24417a9add93204b26a35314be325b8c',1,'CommandController::add(std::vector&lt; AutoCommand * &gt; cmds, double timeout_sec)'],['../classGenericAuto.html#af48629556173af1f5106f7405a196f47',1,'GenericAuto::add(state_ptr new_state)']]],
-  ['add_5fasync_3',['add_async',['../classGenericAuto.html#a938fb69c14735d4cf88ebc7589dcad1c',1,'GenericAuto']]],
-  ['add_5fcancel_5ffunc_4',['add_cancel_func',['../classCommandController.html#addb3119447177b940f071d52afda8234',1,'CommandController']]],
-  ['add_5fdelay_5',['add_delay',['../classCommandController.html#ae06b054f4aeea86e8c6bee56458dc6d1',1,'CommandController::add_delay()'],['../classGenericAuto.html#aaadc99efe49a375f0dc80db7e886d96c',1,'GenericAuto::add_delay()']]],
-  ['add_5fentry_6',['add_entry',['../classMovingAverage.html#afc008077a473e8c8205e2c7832e28ccb',1,'MovingAverage::add_entry()'],['../classExponentialMovingAverage.html#ae6cef331bca0529f93d2d4dfa082fad4',1,'ExponentialMovingAverage::add_entry()']]],
-  ['ang_5faccel_5fdeg_7',['ang_accel_deg',['../classOdometryBase.html#a01f14bfcecbcde7a74b9b98376deb19f',1,'OdometryBase']]],
-  ['ang_5fspeed_5fdeg_8',['ang_speed_deg',['../classOdometryBase.html#a250ceb6ed296ea579e37851e4a0048fb',1,'OdometryBase']]],
-  ['as_5fvector_9',['as_vector',['../classTranslation2d.html#a8d6d449b8eba8e4a6280eef066a1ca49',1,'Translation2d']]],
-  ['async_10',['Async',['../classAsync.html',1,'']]],
-  ['auto_5fdrive_11',['auto_drive',['../classMecanumDrive.html#aa0cfb8683fee99e0a534c3a725d338fd',1,'MecanumDrive']]],
-  ['auto_5fturn_12',['auto_turn',['../classMecanumDrive.html#ac8813aafec14f2d25c3abafefdb81498',1,'MecanumDrive']]],
-  ['autochooser_13',['AutoChooser',['../classAutoChooser.html',1,'AutoChooser'],['../classAutoChooser.html#aa80fcacf1535d1ae098939f84c0967ab',1,'AutoChooser::AutoChooser()']]]
+  ['abstractdevice_1',['AbstractDevice',['../classVDP_1_1AbstractDevice.html',1,'VDP']]],
+  ['accel_2',['accel',['../classOdometryBase.html#a94c814dfb542dd891d84db7e74537249',1,'OdometryBase::accel'],['../structMotionController_1_1m__profile__cfg__t.html#ac4f07f3ac52d12fd820ee4d8ce847f66',1,'MotionController::m_profile_cfg_t::accel']]],
+  ['add_3',['add',['../classCommandController.html#a03fc0dd37c12ae7f43200d5588c768b8',1,'CommandController::add(std::vector&lt; AutoCommand * &gt; cmds)'],['../classCommandController.html#a4cb2c06abd92fb99578f0da75c1411e5',1,'CommandController::add(AutoCommand *cmd, double timeout_seconds=10.0)'],['../classCommandController.html#a24417a9add93204b26a35314be325b8c',1,'CommandController::add(std::vector&lt; AutoCommand * &gt; cmds, double timeout_sec)'],['../classGenericAuto.html#af48629556173af1f5106f7405a196f47',1,'GenericAuto::add(state_ptr new_state)']]],
+  ['add_5fasync_4',['add_async',['../classGenericAuto.html#a938fb69c14735d4cf88ebc7589dcad1c',1,'GenericAuto']]],
+  ['add_5fcancel_5ffunc_5',['add_cancel_func',['../classCommandController.html#addb3119447177b940f071d52afda8234',1,'CommandController']]],
+  ['add_5fdelay_6',['add_delay',['../classCommandController.html#ae06b054f4aeea86e8c6bee56458dc6d1',1,'CommandController::add_delay()'],['../classGenericAuto.html#aaadc99efe49a375f0dc80db7e886d96c',1,'GenericAuto::add_delay()']]],
+  ['add_5fentry_7',['add_entry',['../classMovingAverage.html#afc008077a473e8c8205e2c7832e28ccb',1,'MovingAverage::add_entry()'],['../classExponentialMovingAverage.html#ae6cef331bca0529f93d2d4dfa082fad4',1,'ExponentialMovingAverage::add_entry()']]],
+  ['ang_5faccel_5fdeg_8',['ang_accel_deg',['../classOdometryBase.html#a01f14bfcecbcde7a74b9b98376deb19f',1,'OdometryBase']]],
+  ['ang_5fspeed_5fdeg_9',['ang_speed_deg',['../classOdometryBase.html#a250ceb6ed296ea579e37851e4a0048fb',1,'OdometryBase']]],
+  ['as_5fvector_10',['as_vector',['../classTranslation2d.html#a8d6d449b8eba8e4a6280eef066a1ca49',1,'Translation2d']]],
+  ['async_11',['Async',['../classAsync.html',1,'']]],
+  ['auto_5fdrive_12',['auto_drive',['../classMecanumDrive.html#aa0cfb8683fee99e0a534c3a725d338fd',1,'MecanumDrive']]],
+  ['auto_5fturn_13',['auto_turn',['../classMecanumDrive.html#ac8813aafec14f2d25c3abafefdb81498',1,'MecanumDrive']]],
+  ['autochooser_14',['AutoChooser',['../classAutoChooser.html',1,'AutoChooser'],['../classAutoChooser.html#aa80fcacf1535d1ae098939f84c0967ab',1,'AutoChooser::AutoChooser()']]]
 ];
